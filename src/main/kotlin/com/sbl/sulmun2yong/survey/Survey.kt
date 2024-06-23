157059f46bceff1d528b7bf4c0899036524be809
JeongHunHui <deca3248@gmail.com>
2024-06-24 04:00:40 +0900

    설문 클래스 수정

--- src/main/kotlin/com/sbl/sulmun2yong/survey/Survey.kt
+++ src/main/kotlin/com/sbl/sulmun2yong/survey/Survey.kt
@@ -1,13 +1,32 @@
 package com.sbl.sulmun2yong.survey
 
-import java.util.Date
+import com.sbl.sulmun2yong.response.Answer
+import com.sbl.sulmun2yong.response.Response
+import java.time.LocalDate
 
 data class Survey(
-    private val id: Long,
-    private val title: String,
-    private val description: String,
-    private val sections: List<Section>,
-    private val endDate: Date,
-    private val isPublished: Boolean,
-    private val makerId: Long,
-)
+    val id: Long,
+    val title: String,
+    val description: String,
+    val sections: List<Section>,
+    val endDate: LocalDate,
+    val isPublished: Boolean,
+    val makerId: Long,
+    val drawId: Long?,
+) {
+    fun participate(
+        id: Long,
+        participantId: Long,
+        answers: List<Answer>,
+    ): Response {
+        if (!isPublished) {
+            throw IllegalStateException("Cannot participate in an unpublished survey")
+        }
+
+        if (LocalDate.now() > endDate) {
+            throw IllegalStateException("Cannot participate in a survey that has ended")
+        }
+
+        return Response(id, this.id, participantId, answers)
+    }
+}
